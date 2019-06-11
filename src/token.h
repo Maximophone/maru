566ec09f7218d6d49244170acb149b7dcf748332
Maximux <fournes.maxime@gmail.com>
2019-06-12 00:04:53 +0100

    Initial Commit

---
+++ src/token.h
@@ -0,0 +1,21 @@
+typedef char TokenType[10];
+
+const TokenType ILLEGAL = "ILLEGAL";
+const TokenType EOF = "EOF";
+
+const TokenType IDENT = "IDENT";
+const TokenType INT = "INT";
+
+const TokenType ASSIGN = "ASSIGN";
+const TokenType PLUS = "PLUS";
+
+const TokenType COMMA = "COMMA";
+const TokenType SEMICOLON = "SEMICOLON";
+const TokenType LPAREN = "LPAREN";
+const TokenType RPAREN = "RPAREN";
+const TokenType LBRACE = "LBRACE";
+const TokenType RBRACE = "RBRACE";
+
+const TokenType FUNCTION = "FUNCTION";
+const TokenType LET = "LET";
+
